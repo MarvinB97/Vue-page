c805cc9885c11f5c2e46c6501071d471f1346316
MarvinB97 <mb.jobtoday@gmail.com>
2023-02-23 18:33:12 -0500

    Mod

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 const App = new Vue({
     el: "#app",
     data: {
-        title: "Title"
+        title: "Title",
+        listAccordion:['First-Accordion','Second-Accordion','Third-Accordion', 'Fourth-Accordion'],
+        textAccordion:['text-one','text-two','text-three','text-four'],
     },
     methods:{
-
+        firstFunction:function(ind,it,content){}
     }
 })
